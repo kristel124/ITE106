544b0076b5ac94537277d6bfc3bec43f42fcc68c
kristel124 <mkristel178@gmail.com>
2024-11-22 12:22:19 +0800

    Delete MenuExample/Act4 directory

--- MenuExample/Act4/MenuActionListener.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.swing.*;
-
-public class MenuActionListener implements ActionListener {
-	public void actionPerformed(ActionEvent e){
-		JoptionPane.showMessageDiaglog(MenuExample.frame, "Got an ActionEvent at " + new Date(e.getWhen()) + "from" + e.getSource().getClass());
-	}
-}
